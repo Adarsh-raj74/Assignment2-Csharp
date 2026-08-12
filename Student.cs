5bd0fe3918f13666dd0b8f7adc34037b58282f57
Adarsh rajput <adarshraj7457@gmail.com>
2026-08-12 14:02:35 +0530

    Add files via upload

--- Student.cs
+++ Student.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-
-namespace AccessModifierLab
+namespace StudentLibrary
 {
-    public class Student3
+    public class Student
     {
         public string name = "Adarsh";
         private int age = 22;
         protected string course = "MCA";
         internal string college = "GLA University";
-        protected internal string city = "Mathura";
 
-        public void ShowStudent()
+        public void ShowData()
         {
-            Console.WriteLine("Student Name: " + name);
+            Console.WriteLine("Name: " + name);
             Console.WriteLine("Age: " + age);
             Console.WriteLine("Course: " + course);
             Console.WriteLine("College: " + college);
-            Console.WriteLine("City: " + city);
         }
     }
 }
