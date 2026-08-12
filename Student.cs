f6e2c1d3633c6a1712b03291afe367932e000c13
Adarsh rajput <adarshraj7457@gmail.com>
2026-08-12 14:22:49 +0530

    Add files via upload
    StudentLibrary/
    │
    ├── StudentLibrary.sln
    │
    ├── StudentLibrary/
    │   ├── StudentLibrary.csproj
    │   └── Student.cs
    │
    └── StudentClient/
        ├── StudentClient.csproj
        ├── Program.cs
        └── ResearchStudent.cs

--- Student.cs
+++ Student.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+
 namespace StudentLibrary
 {
     public class Student
     {
         public string name = "Adarsh";
+
         private int age = 22;
+
         protected string course = "MCA";
+
         internal string college = "GLA University";
+
+        protected internal string department = "Computer Science";
+
+        private protected string rollNo = "MCA101";
 
         public void ShowData()
         {
@@ -14,6 +22,8 @@ namespace StudentLibrary
             Console.WriteLine("Age: " + age);
             Console.WriteLine("Course: " + course);
             Console.WriteLine("College: " + college);
+            Console.WriteLine("Department: " + department);
+            Console.WriteLine("Roll No: " + rollNo);
         }
     }
 }
